49eb2157c87a8e42f6c667e3727f70ee9a8f23a9
mzeecob <reshimwe2025@gmail.com>
2019-10-03 19:32:57 +0200

    classes in kotlin

---
+++ src/Classes.kt
@@ -0,0 +1,12 @@
+
+// class with constructive and getter and setters at same time
+
+class Car (val make: String, val model: String){
+
+}
+
+fun main() {
+
+    val car = Car("Toyota", "Avalon")
+
+}
