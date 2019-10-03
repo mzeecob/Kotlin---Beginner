6eaf7363497d4e7ad8b5fd4ddd40730fafa85e77
mzeecob <reshimwe2025@gmail.com>
2019-10-03 15:10:04 +0200

    conditions in kotlin

---
+++ src/Conditional.kt
@@ -0,0 +1,14 @@
+
+
+fun main() {
+
+//    using switch/when cases
+    val x = 1
+
+    when (x){
+        1 -> println("x is 1")
+        2 -> println("x is 2")
+        else -> println("x is nothing")
+
+    }
+}
